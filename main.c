255889d114b2430f75774c645cb42429ff0c99f4
Ambroise Mostin <40889985+amostin@users.noreply.github.com>
2019-07-08 22:32:32 +0200

    nombre aléatoire different
    le nombre aléatoire est bien différent entre 2 compil

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
+//lib utile au printf()
 #include <stdio.h>
+//lib utile au time()
+#include <time.h>
+//lib utile au srand()
+#include <stdlib.h>
 
 int main () {
+    //un temps aléatoire
+    time_t t;
+    //generation d'un nouveau nbre aleatoire parce que sinon ya que le premier qui est aleatoire puis le reste est le meme
+    srand((unsigned) time(&t));
+    //fct qui prend deux nbre en entrée et retourne  un nbre entre les deux entrés
     int genere_sec_entre_min_max(int min, int max) { return rand() % (max + 2 - min) + min; }
+    //sec contient un chiffre entre 35 et 40
     int sec = genere_sec_entre_min_max(35, 40);
+    //affiche le nombre contenu dans sec
     printf("%d", sec);
+    //retourne un chiffre car c'est ce que prédit le int devant main()
     return(0);
 }
