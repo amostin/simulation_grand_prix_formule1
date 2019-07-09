4beddf1476c213990233fe73d3c73e30b61363f9
Ambroise Mostin <40889985+amostin@users.noreply.github.com>
2019-07-09 16:20:26 +0200

    CONDITION DE DÉPART

--- main.c
+++ main.c
@@ -29,12 +29,18 @@
             return tour;
         }
 
+        void ecrit_classement_final(){
+            printf("\n ICI JE VAIS ECRIRE LE FICHIER");
+        }
+
     //affiche les titres des colonnes avec separatuers et insere des valeurs dedans
     void affiche(){
         //Je veux afficher:
         //name|s1|s2|s3|tour|bestour|pit|out
         //----|--|--|--|----|-------|---|---
         //22|38|37|41|116|105|no|no
+        char titres_colonnes[] = "name|s1|s2|s3|tour|bestour|pit|out\n";
+        char separateur_titres_valeurs[] = "----|--|--|--|----|-------|---|---\n";
 
         int name = genere_sec_entre_min_max(10, 22);
         int s1 = genere_sec_entre_min_max(35, 40);
@@ -44,17 +50,20 @@
         //je veux calculer s1+s2+s3 et retourner le resultat
         int tour = calculTour(s1, s2, s3);
         int bestour =  calculTour(s1, s2, s3);
-        char titres_colonnes[] = "name|s1|s2|s3|tour|bestour|pit|out\n";
-        char separateur_titres_valeurs[] = "----|--|--|--|----|-------|---|---\n";
+
         printf("%s", titres_colonnes);
         printf("%s", separateur_titres_valeurs);
         printf("%d  |%d|%d|%d|%d |%d    |no |no ", name, s1, s2, s3, tour, bestour);
+
+        ecrit_classement_final();
     }
 
 int main (int argc, char *argv[]) {
     //petit rappel argc argv comme ça on sais comment gerer les parametre du programme quand on en aura besoin
     if (argc == 1){printf("Le seul argument est le: %s", argv[argc-1]); return 69;}
     else if (argc == 2){printf("Le premier argument est le: %s\nLe deuxieme est: %s", argv[0], argv[1]); return 70;}
+
+
     affiche();
     //retourne un chiffre car c'est ce que prédit le int devant main()
     return(0);
