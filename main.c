ca3e49b331013a121920b54ac6f3c39ef8080cc6
Ambroise Mostin <40889985+amostin@users.noreply.github.com>
2019-07-14 17:40:14 +0200

    2 voitures qui affichent s1s2s3 dans tableau
    j'ai pas encore mis le reste car j'ai peur que ça marche pas direct donc je fais petit à petit

--- main.c
+++ main.c
@@ -10,92 +10,73 @@
 #include <sys/types.h>
 /* Pour wait() */
 #include <sys/wait.h>
+
 // variable globale pour pouvoir changer changer l'intervalle facilement
 int minimum = 35;
 int maximum = 40;
-//Pour éviter implicit declaration of function invalid in C99
-//int genere_sec_entre_min_max(int min, int max);
+
 //fct qui prend deux nbre en entrée et retourne  un nbre entre les deux entrés. attention lors des tests il renvoi meme un nombre au dessus du max
 int genere_sec_entre_min_max(int min, int max) {
-    //un temps aléatoire
-    //time_t t;
-    //generation d'un nouveau nbre aleatoire parce que sinon ya que le premier qui est aleatoire puis le reste est le meme
-    //srand((unsigned) time(&t));
     //le % defini le max puis le calcul defini le min
     int sec = rand() % (max + 2 - min) + min;
-    //affiche le nombre contenu dans sec
-    //printf("%d", sec);
-    //retourne le chiffre aléatoire entre min et max
+    //retourne le chiffre aléatoire entre min et max+1
     return sec;
 }
 
 /* La fonction create_process duplique le processus appelant et retourne
    le PID du processus fils ainsi créé */
-pid_t create_process(void)
-{
+pid_t create_process(void){
     /* On crée une nouvelle valeur de type pid_t */
     pid_t pid;
-
     /* On fork() tant que l'erreur est EAGAIN */
-    do {
-        pid = fork();
-    } while ((pid == -1) && (errno == EAGAIN));
-
+    do {pid = fork();} while ((pid == -1) && (errno == EAGAIN));
     /* On retourne le PID du processus ainsi créé */
     return pid;
 }
 
 /* La fonction child_process effectue les actions du processus fils */
-void child_process(void)
-{
+void child_process(void){
     //on initialise le random pour chaque process
     srand(getpid());
-    printf(" Nous sommes dans le fils !\n"
-           " Le PID du fils est %d.\n"
-           " Le PPID du fils est %d.\n", (int) getpid(), (int) getppid());
-    //laisse le temps pour kill pid dans un autre terminal jle laisse au cas où ça serait utile
-    //sleep(20);
-    /*if (genere_sec_entre_min_max(minimum, maximum) > maximum){
-        printf("Secteur 1: %d\n", (genere_sec_entre_min_max(minimum, maximum)-1));
-    } else if (genere_sec_entre_min_max(minimum, maximum) < minimum){
-        printf("Secteur 1: %d\n", (genere_sec_entre_min_max(minimum, maximum)+1));
-    } else {
-        printf("Secteur 1: %d\n", genere_sec_entre_min_max(minimum, maximum));
-    }*/
-    
+    printf(" Nous sommes dans le fils !\n");
+
     char titres_colonnes[] = "num|s1|s2|s3|tour  |bestour|pit|out|numTour|Tot\n";
     char separateur_titres_valeurs[] = "---|--|--|--|------|-------|---|---|-------|---\n";
+
+    int s1 = genere_sec_entre_min_max(minimum, maximum);
+    int s2 = genere_sec_entre_min_max(minimum, maximum);
+    int s3 = genere_sec_entre_min_max(minimum, maximum);
+
     printf("%s", titres_colonnes);
     printf("%s", separateur_titres_valeurs);
-    
-    printf("Secteur 1: %d\n", genere_sec_entre_min_max(minimum, maximum));
-    printf("Secteur 2: %d\n", genere_sec_entre_min_max(minimum, maximum));
-    printf("Secteur 3: %d\n", genere_sec_entre_min_max(minimum, maximum));
+    printf("num|%d|%d|%d|\n", s1, s2, s3);
 }
 
 /* La fonction father_process effectue les actions du processus père */
-void father_process(int child_pid)
-{
+void father_process(int child_pid){
     //indique l'etat si error ou terminaison normale
     int status;
     //on initialise le random pour chaque process
     srand(getpid());
+    printf(" Nous sommes dans le père !\n");
 
-    printf(" Nous sommes dans le père !\n"
-           " Le PID du fils est %d.\n"
-           " Le PID du père est %d.\n", (int) child_pid, (int) getpid());
-    printf("Secteur 1: %d\n", genere_sec_entre_min_max(minimum, maximum));
-    printf("Secteur 2: %d\n", genere_sec_entre_min_max(minimum, maximum));
-    printf("Secteur 3: %d\n", genere_sec_entre_min_max(minimum, maximum));
+    char titres_colonnes[] = "num|s1|s2|s3|tour  |bestour|pit|out|numTour|Tot\n";
+    char separateur_titres_valeurs[] = "---|--|--|--|------|-------|---|---|-------|---\n";
 
+    int s1 = genere_sec_entre_min_max(minimum, maximum);
+    int s2 = genere_sec_entre_min_max(minimum, maximum);
+    int s3 = genere_sec_entre_min_max(minimum, maximum);
+
+    printf("%s", titres_colonnes);
+    printf("%s", separateur_titres_valeurs);
+    printf("num|%d|%d|%d|\n", s1, s2, s3);
 
     if (wait(&status) == -1) {perror("wait :");exit(EXIT_FAILURE);}
     if (WIFEXITED(status)) {printf(" Terminaison normale du processus fils.\n Code de retour : %d.\n", WEXITSTATUS(status));}
     if (WIFSIGNALED(status)) {printf(" Terminaison anormale du processus fils.\n Tué par le signal : %d.\n", WTERMSIG(status));}
 }
 
-int main(void)
-{
+int main(void){
     pid_t pid = create_process();
 
     switch (pid) {
