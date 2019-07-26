1cbc17692a9c9fff60e043d1adfe5eede2b44674
ATHOOS <43479148+ATHOOS@users.noreply.github.com>
2019-07-26 13:42:53 +0200

    memoire partagee wip
    memoire pas encore terminee

--- main.c
+++ main.c
@@ -13,19 +13,68 @@
 //utile au strcat()
 #include <string.h>
 #include <time.h>
+#include <sys/shm.h>
 #include "Utils.h"
 
 
 int main() {
-    double test = genS1()/(double)1000;
-    double test2 = genS2()/(double)1000;
-    double test3 = genS3()/(double)1000;
-    printf("%.3f\n", test);
-    printf("%.3f\n", test2);
-    printf("%.3f\n", test3);
+    int ID[20] = {44, 77, 5, 7, 3, 33, 11, 31, 18, 35, 27, 55, 10, 28, 8, 20, 2, 14, 9, 16};
 
-    double tour = calculTour(test,test2,test3);
-    printf("%.3f\n", tour);
+    int shmid;
+    key_t key;
+    key = 5678;
 
-    printf("%s\n", timeFormat(tour*1000));
+    Voiture *course;
+
+    shmid = shmget(key, 20*sizeof(Voiture), IPC_CREAT|666);
+
+    if (shmid == -1) {
+        perror("shmget");
+        exit(1);
+    }
+
+    course = shmat(shmid, 0, 0);
+
+    if (course == (Voiture *)-1) {
+        perror("shmat");
+        exit(1);
+    }
+
+
+
+    for (int i=0; i<20; i++) {
+        pid_t pid;
+        pid = fork();
+
+        if (pid == 0) {
+            srand(getpid());
+
+            course = shmat(shmid, 0, 0);
+
+            if (course == (Voiture *)-1) {
+                perror("shmat");
+                exit(1);
+            }
+            course[i]->id = ID[i];
+            course[i]->s1 = genS1();
+            course[i]->s2 = genS2();
+            course[i]->s3 = genS3();
+            course[i]->tour = calculTour(course[i]->s1, course[i]->s2, course[i]->s3);
+            printf("%d\n", course[i]->id);
+            printf("%.3f\n", (course[i]->s1)/(double)1000);
+            printf("%.3f\n", (course[i]->s2)/(double)1000);
+            printf("%.3f\n", (course[i]->s3)/(double)1000);
+            printf("%s\n" , timeFormat(course[i]->tour));
+
+
+
+            exit(0);
+        } else {
+
+            sleep(1);
+        }
+    }
 }
+
+
+
